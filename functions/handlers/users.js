1fecb0bce3bc9b24de11e24e19f2bc9adc8523cc
Bartosz <radu480@o2.pl>
2021-07-29 12:44:37 +0200

    Modifed respons for sucessfull sign up.

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -29,7 +29,9 @@ exports.signUp = (req, res) => {
       });
     })
     .then(() => {
-      return res.status(200);
+      return res
+        .status(200)
+        .json({ message: "Sign up completed successfully." });
     })
     .catch((error) => {
       return res.status(500).json(error);
