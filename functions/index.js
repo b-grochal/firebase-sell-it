ababac4db260c1c885a4df6e1d2d370a27410fbc
Bartosz <radu480@o2.pl>
2021-08-12 17:09:56 +0200

    Fixed bugs with users' methods.

--- functions/index.js
+++ functions/index.js
@@ -2,12 +2,13 @@ const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
 
-const { signUp } = require("./handlers/users");
+const { signUp, signIn } = require("./handlers/users");
 
 const app = express();
 app.use(cors());
 
 app.post("/sign-up", signUp);
+app.post("/sign-in", signIn);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
